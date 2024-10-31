a585eb1ff7024cbc0ce8ce020b4b37d91ce436d0
Trent Hornibrook <trent.hornibrook@bjss.com>
2024-10-31 15:33:53 +1100

    [skip ci] Add example terraform config

---
+++ tf/terraform.tf
@@ -0,0 +1,11 @@
+# Configure the Azure provider
+terraform {
+  required_version = "~> 1.9"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.5.0"
+    }
+  }
+}
+
